325ceb6adbca45289e4d7ff820bcaa62f818aa56
olukotun <olukotuno@gmail.com>
2020-05-18 21:51:44 -0400

    Updates

---
+++ precache-manifest.39b60f1564c79058fcd641c9e31999e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7b084aec8f724225b228cda129dec2",
+    "url": "/monster-rolex/index.html"
+  },
+  {
+    "revision": "5a258df377b0edacb72f",
+    "url": "/monster-rolex/static/css/main.74197b09.chunk.css"
+  },
+  {
+    "revision": "ffb6fcfc414d79e79683",
+    "url": "/monster-rolex/static/js/2.54b49b61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolex/static/js/2.54b49b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a258df377b0edacb72f",
+    "url": "/monster-rolex/static/js/main.4f98c04b.chunk.js"
+  },
+  {
+    "revision": "f501b3856630e8197b3c",
+    "url": "/monster-rolex/static/js/runtime-main.0b9351c2.js"
+  }
+]);
